2680446649dbdbdb82f98b8fcff6e54743d262a2
shubham jangid <sj9972382@gmail.com>
2026-03-11 18:36:06 +0530

    fix: add requests dependency

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -189,3 +189,14 @@ ACCOUNT_FORMS = {}
 
 # Apply CSS classes to all allauth form fields automatically
 ACCOUNT_SIGNUP_FORM_CLASS = None
+
+from django.contrib.auth import get_user_model
+
+if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
