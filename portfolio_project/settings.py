882e5b0ca814df80a4382f8166f3f419cc43edfb
shubham jangid <sj9972382@gmail.com>
2026-03-14 10:20:18 +0530

    fix: add requests dependency

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -110,7 +110,9 @@ else:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
